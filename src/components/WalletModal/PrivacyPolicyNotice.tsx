c77127e202aa92ab32fdf25aab3dc7e17416dbf0
Sterling <124077515+sterling-ly@users.noreply.github.com>
2023-04-17 18:14:59 -0400

    text fix before connecting wallet

--- src/components/WalletModal/PrivacyPolicyNotice.tsx
+++ src/components/WalletModal/PrivacyPolicyNotice.tsx
@@ -1,34 +1,12 @@
+/* eslint-disable import/no-unused-modules */
 import { Trans } from '@lingui/macro'
-import styled from 'styled-components/macro'
-import { ExternalLink, ThemedText } from 'theme'
-
-const StyledLink = styled(ExternalLink)`
-  font-weight: 600;
-  color: ${({ theme }) => theme.textSecondary};
-`
-
-const LastUpdatedText = styled.span`
-  color: ${({ theme }) => theme.textTertiary};
-`
-
-const LAST_UPDATED_DATE = '11.17.22'
+//import styled from 'styled-components/macro'
+import { ThemedText } from 'theme'
 
 export default function PrivacyPolicyNotice() {
   return (
     <ThemedText.Caption color="textSecondary">
-      <Trans>By connecting a wallet, you agree to Uniswap Labs&apos;</Trans>{' '}
-      <StyledLink href="https://uniswap.org/terms-of-service/">
-        <Trans>Terms of Service</Trans>{' '}
-      </StyledLink>
-      <Trans>and consent to its</Trans>{' '}
-      <StyledLink href="https://uniswap.org/privacy-policy">
-        <Trans>Privacy Policy.</Trans>
-      </StyledLink>
-      <LastUpdatedText>
-        {' ('}
-        <Trans>Last Updated</Trans>
-        {` ${LAST_UPDATED_DATE})`}
-      </LastUpdatedText>
+      <Trans>Trade responsibly</Trans>
     </ThemedText.Caption>
   )
 }
